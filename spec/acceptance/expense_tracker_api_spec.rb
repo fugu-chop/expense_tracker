51d2c9d1a324db96eb5afd95cd27cf0b7c4717c2
Dean Wan <deanwan14@gmail.com>
2023-02-05 14:27:44 +1100

    docs: add more comments on code sources

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -7,13 +7,15 @@ module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
 
-    it 'records submitted expenses' do 
+    it 'records submitted expenses' do
+      # Strings hash keys for compatibility
       coffee = {
         'payee' => 'Starbucks',
         'amount' => 5.75,
         'date' => '2017-06-10'
       }
     
+      # post method comes from Rack::Test::Methods
       post '/expenses', JSON.generate(coffee)
     end
   end 
