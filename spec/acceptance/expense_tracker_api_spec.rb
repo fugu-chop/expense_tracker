b4c08f1ead2a0c0e3e01f8afc74a95a17fcea1e9
Dean Wan <deanwan14@gmail.com>
2023-02-06 10:32:40 +1100

    refactor: move expectations into method

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -12,20 +12,25 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
+    def post_expense(expense)
+      # post method comes from Rack::Test::Methods
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+      
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      # merge the id into the JSON body so that we
+      # can use whatever id the database creates
+      expense.merge('id' => parsed['expense_id'])
+    end
+
     it 'records submitted expenses' do
       # Strings hash keys for compatibility
-      coffee = {
+      coffee = post_expense(
         'payee' => 'Starbucks',
         'amount' => 5.75,
         'date' => '2017-06-10'
-      }
-    
-      # post method comes from Rack::Test::Methods
-      post '/expenses', JSON.generate(coffee)
-      expect(last_response.status).to eq(200)
-
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      )
     end
   end 
 end
