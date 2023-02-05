110a3c03a74d382599f9523cd88f6837b87d5180
Dean Wan <deanwan14@gmail.com>
2023-02-05 14:26:14 +1100

    chore: add config to log failures to disk

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    config.example_status_persistence_file_path = 'example_failures.txt'
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
