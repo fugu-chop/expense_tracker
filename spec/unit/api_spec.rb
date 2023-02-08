6291516e859f7a03f8d44899b867bd39502b5799
Dean Wan <deanwan14@gmail.com>
2023-02-08 16:36:44 +1100

    test: fill in test for post expense

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -1,10 +1,36 @@
 require_relative '../../app/api.rb'
+require 'rack/test'
 
 module ExpenseTracker
-  RSpec.describe do
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+  
+  RSpec.describe API do
+    # Rack::Test allows us to simulate and route
+    # HTTP requests to and from our app
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id'
+        it 'returns the expense id' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          
+          expect(parsed).to include('expense_id' => 417)
+        end
+        
         it 'responds with a 200 (OK)'
       end
 
