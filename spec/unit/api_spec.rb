f5331adfef2646fd35e770f59d8a4430e5f3230c
Dean Wan <deanwan14@gmail.com>
2023-02-08 18:22:18 +1100

    test: update unhappy path tests

--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -14,11 +14,10 @@ module ExpenseTracker
     end
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+    let(:expense) { { 'some' => 'data' } }
 
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        let(:expense) { { 'some' => 'data' } }
-
         before do
             allow(ledger).to receive(:record)
               .with(expense)
@@ -39,6 +38,27 @@ module ExpenseTracker
           post '/expenses', JSON.generate(expense)
 
           expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the expense fails validation' do
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense incomplete')
+        end
+        
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+
+          expect(last_response.status).to eq(422)
         end
       end
     end
