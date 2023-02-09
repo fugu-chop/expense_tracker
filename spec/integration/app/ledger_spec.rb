6247a830c0bbc76ce8548b8aea83197f438d91c5
Dean Wan <deanwan14@gmail.com>
2023-02-09 16:05:54 +1100

    chore: clean up comments

--- spec/integration/app/ledger_spec.rb
+++ spec/integration/app/ledger_spec.rb
@@ -14,15 +14,9 @@ module ExpenseTracker
 
     describe '#record' do
       context 'with a valid expense' do
-        # :aggregate_failures allows the spec to continue
-        # running despite failing on the first expectation
         it 'successfully saves the expense in the DB' do
           result = ledger.record(expense)
 
-          # Generally not good practice to have multiple
-          # expectations in a single unit spec, but since
-          # we're touching the database, such specs are
-          # going to be slower for setup and teardown
           expect(result).to be_success
           expect(DB[:expenses].all).to match [a_hash_including(
             id: result.expense_id,
